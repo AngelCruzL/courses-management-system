33ed3772017a92a49c057c1a1230e3d6f604554c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-14 10:33:42 -0600

    Add User entity data annotations and utility methods

--- src/main/java/dev/angelcruzl/courseapp/entity/User.java
+++ src/main/java/dev/angelcruzl/courseapp/entity/User.java
@@ -1,15 +1,39 @@
 package dev.angelcruzl.courseapp.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
   private Long id;
+
+  @Basic
+  @Column(name = "email", nullable = false, length = 50, unique = true)
   private String email;
+
+  @Basic
+  @Column(name = "password", nullable = false, length = 64)
   private String password;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+    name = "users_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
   private Set<Role> roles = new HashSet<>();
+
+  @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
   private Student student;
+
+  @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
   private Instructor instructor;
 
   public User(String email, String password) {
@@ -18,6 +42,16 @@ public class User {
   }
 
   public User() {
+  }
+
+  public void assignRoleToUser(Role role){
+    this.roles.add(role);
+    role.getUsers().add(this);
+  }
+
+  public void removeRoleFromUser(Role role){
+    this.roles.remove(role);
+    role.getUsers().remove(this);
   }
 
   @Override
