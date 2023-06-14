d67b81dd85673eed868c41abcdf334d97b2c43f2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-14 10:36:09 -0600

    Add Role entity data annotations

--- src/main/java/dev/angelcruzl/courseapp/entity/Role.java
+++ src/main/java/dev/angelcruzl/courseapp/entity/Role.java
@@ -1,12 +1,24 @@
 package dev.angelcruzl.courseapp.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "roles")
 public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
   private Long id;
+
+  @Basic
+  @Column(name = "name", nullable = false, length = 45, unique = true)
   private String name;
+
+  @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
   private Set<User> users = new HashSet<>();
 
   public Role(String name) {
