cbdff01db77b3427d7418b11bad8ccce7a2aca4e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-14 04:29:27 -0600

    Add Role and User entities relationships

--- src/main/java/dev/angelcruzl/courseapp/entity/Role.java
+++ src/main/java/dev/angelcruzl/courseapp/entity/Role.java
@@ -1,10 +1,52 @@
 package dev.angelcruzl.courseapp.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Role {
   private Long id;
   private String name;
+  private Set<User> users = new HashSet<>();
+
+  public Role(String name) {
+    this.name = name;
+  }
+
+  public Role() {
+  }
+
+  @Override
+  public String toString() {
+    return "Role{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      '}';
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<User> getUsers() {
+    return users;
+  }
+
+  public void setUsers(Set<User> users) {
+    this.users = users;
+  }
 
   @Override
   public boolean equals(Object o) {
--- src/main/java/dev/angelcruzl/courseapp/entity/User.java
+++ src/main/java/dev/angelcruzl/courseapp/entity/User.java
@@ -1,11 +1,81 @@
 package dev.angelcruzl.courseapp.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class User {
   private Long id;
   private String email;
   private String password;
+  private Set<Role> roles = new HashSet<>();
+  private Student student;
+  private Instructor instructor;
+
+  public User(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public User() {
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "id=" + id +
+      ", email='" + email + '\'' +
+      ", password='" + password + '\'' +
+      '}';
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
+
+  public Student getStudent() {
+    return student;
+  }
+
+  public void setStudent(Student student) {
+    this.student = student;
+  }
+
+  public Instructor getInstructor() {
+    return instructor;
+  }
+
+  public void setInstructor(Instructor instructor) {
+    this.instructor = instructor;
+  }
 
   @Override
   public boolean equals(Object o) {
