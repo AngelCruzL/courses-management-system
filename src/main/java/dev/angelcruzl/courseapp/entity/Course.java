14101889dfa245a402f18d23532a4c0e7513960a
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-14 04:24:18 -0600

    Add Course, Instructor and Student entities relationships

--- src/main/java/dev/angelcruzl/courseapp/entity/Course.java
+++ src/main/java/dev/angelcruzl/courseapp/entity/Course.java
@@ -1,12 +1,89 @@
 package dev.angelcruzl.courseapp.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Course {
   private Long id;
   private String title;
   private String description;
   private String duration;
+  private Instructor instructor;
+  private Set<Student> students = new HashSet<>();
+
+  public Course(
+    String title,
+    String description,
+    String duration,
+    Instructor instructor
+  ) {
+    this.title = title;
+    this.description = description;
+    this.duration = duration;
+    this.instructor = instructor;
+  }
+
+  public Course() {
+  }
+
+  @Override
+  public String toString() {
+    return "Course{" +
+      "id=" + id +
+      ", title='" + title + '\'' +
+      ", description='" + description + '\'' +
+      ", duration='" + duration + '\'' +
+      '}';
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDuration() {
+    return duration;
+  }
+
+  public void setDuration(String duration) {
+    this.duration = duration;
+  }
+
+  public Instructor getInstructor() {
+    return instructor;
+  }
+
+  public void setInstructor(Instructor instructor) {
+    this.instructor = instructor;
+  }
+
+  public Set<Student> getStudents() {
+    return students;
+  }
+
+  public void setStudents(Set<Student> students) {
+    this.students = students;
+  }
 
   @Override
   public boolean equals(Object o) {
--- src/main/java/dev/angelcruzl/courseapp/entity/Student.java
+++ src/main/java/dev/angelcruzl/courseapp/entity/Student.java
@@ -1,12 +1,89 @@
 package dev.angelcruzl.courseapp.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Student {
   private Long id;
   private String firstName;
   private String lastName;
   private String level;
+  private Set<Course> courses = new HashSet<>();
+  private User user;
+
+  public Student(
+    String firstName,
+    String lastName,
+    String level,
+    User user
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.level = level;
+    this.user = user;
+  }
+
+  public Student() {
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+      "id=" + id +
+      ", firstName='" + firstName + '\'' +
+      ", lastName='" + lastName + '\'' +
+      ", level='" + level + '\'' +
+      '}';
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getLevel() {
+    return level;
+  }
+
+  public void setLevel(String level) {
+    this.level = level;
+  }
+
+  public Set<Course> getCourses() {
+    return courses;
+  }
+
+  public void setCourses(Set<Course> courses) {
+    this.courses = courses;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
 
   @Override
   public boolean equals(Object o) {
