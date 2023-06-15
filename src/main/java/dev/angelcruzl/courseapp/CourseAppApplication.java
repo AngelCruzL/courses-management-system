40267d4292a21d8f5284cd3c30faa248e8d3e2b2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-15 08:22:02 -0600

    Remove OperationUtility helper class and clean up CourseAppApplication file

--- src/main/java/dev/angelcruzl/courseapp/CourseAppApplication.java
+++ src/main/java/dev/angelcruzl/courseapp/CourseAppApplication.java
@@ -1,33 +1,13 @@
 package dev.angelcruzl.courseapp;
 
-import dev.angelcruzl.courseapp.repository.*;
-import dev.angelcruzl.courseapp.utility.OperationUtility;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseAppApplication implements CommandLineRunner {
-  @Autowired
-  private UserRepository userRepository;
-  @Autowired
-  private CourseRepository courseRepository;
-  @Autowired
-  private InstructorRepository instructorRepository;
-  @Autowired
-  private StudentRepository studentRepository;
-  @Autowired
-  private RoleRepository roleRepository;
+public class CourseAppApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CourseAppApplication.class, args);
   }
 
-  @Override
-  public void run(String... args) throws Exception {
-    OperationUtility.usersOperations(userRepository);
-    OperationUtility.rolesOperations(roleRepository, userRepository);
-//    OperationUtility.assignRolesToUsers(roleRepository, userRepository);
-  }
 }
