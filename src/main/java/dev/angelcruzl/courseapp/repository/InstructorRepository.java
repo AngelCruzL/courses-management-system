b6c891ffea683309f45dc9d171ee563014862c02
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-15 08:43:24 -0600

    Refactor Instructor and Student queries return type from list to pagination

--- src/main/java/dev/angelcruzl/courseapp/repository/InstructorRepository.java
+++ src/main/java/dev/angelcruzl/courseapp/repository/InstructorRepository.java
@@ -1,15 +1,15 @@
 package dev.angelcruzl.courseapp.repository;
 
 import dev.angelcruzl.courseapp.entity.Instructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface InstructorRepository extends JpaRepository<Instructor,Long> {
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
   @Query(value = "SELECT i FROM Instructor AS i WHERE i.firstName LIKE %:name% OR i.lastName LIKE %:name%")
-  List<Instructor> findInstructorsByName(@Param("name") String name);
+  Page<Instructor> findInstructorsByName(@Param("name") String name, PageRequest pageRequest);
 
   @Query(value = "SELECT i FROM Instructor AS i WHERE i.user.email =:email")
   Instructor findInstructorByEmail(@Param("email") String email);
--- src/main/java/dev/angelcruzl/courseapp/repository/StudentRepository.java
+++ src/main/java/dev/angelcruzl/courseapp/repository/StudentRepository.java
@@ -1,15 +1,15 @@
 package dev.angelcruzl.courseapp.repository;
 
 import dev.angelcruzl.courseapp.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
   @Query(value = "SELECT s FROM Student AS s WHERE s.firstName LIKE %:name% OR s.lastName LIKE %:name%")
-  List<Student> findStudentsByName(@Param("name") String name);
+  Page<Student> findStudentsByName(@Param("name") String name, PageRequest pageRequest);
 
   @Query(value = "SELECT s FROM Student AS s WHERE s.user.email =:email")
   Student findStudentByEmail(String email);
