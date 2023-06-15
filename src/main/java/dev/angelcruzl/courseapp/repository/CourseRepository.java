7d304407130570c0c2d32afb8156b7f2f4028881
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-15 08:37:23 -0600

    Refactor Course queries return type from list to pagination

--- src/main/java/dev/angelcruzl/courseapp/repository/CourseRepository.java
+++ src/main/java/dev/angelcruzl/courseapp/repository/CourseRepository.java
@@ -1,15 +1,21 @@
 package dev.angelcruzl.courseapp.repository;
 
 import dev.angelcruzl.courseapp.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface CourseRepository extends JpaRepository<Course, Long> {
-  List<Course> findCoursesByTitleContains(String keyword);
+  Page<Course> findCoursesByTitleContains(String keyword, Pageable pageable);
 
   @Query(value = "SELECT * FROM courses AS c WHERE c.id IN (SELECT e.course_id FROM enrolled_in AS e WHERE e.student_id =:studentId)", nativeQuery = true)
-  List<Course> getCoursesByStudentId(@Param("studentId") Long studentId);
+  Page<Course> getCoursesByStudentId(@Param("studentId") Long studentId, Pageable pageable);
+
+  @Query(value = "SELECT *  FROM courses AS c WHERE c.id NOT IN (SELECT e.course_id FROM enrolled_in AS e WHERE e.student_id =:studentId)", nativeQuery = true)
+  Page<Course> getNonEnrolledInCoursesByStudentId(@Param("studentId") Long studentId, Pageable pageable);
+
+  @Query(value = "SELECT c FROM Course AS c WHERE c.instructor.id =:instructorId")
+  Page<Course> getCoursesByInstructorId(@Param("instructorId") Long instructorId, Pageable pageable);
 }
