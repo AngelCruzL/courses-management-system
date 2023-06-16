48b98cb9e60874b5a062b1a348beb281161135fb
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-15 18:26:37 -0600

    Add UserRestController interface and implementation

---
+++ src/main/java/dev/angelcruzl/courseapp/web/UserRestController.java
@@ -0,0 +1,20 @@
+package dev.angelcruzl.courseapp.web;
+
+import dev.angelcruzl.courseapp.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+  private final UserService userService;
+
+  public UserRestController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/users")
+  public boolean checkIfEmailExists(@RequestParam(name = "email", defaultValue = "") String email) {
+    return userService.loadUserByEmail(email) != null;
+  }
+}
